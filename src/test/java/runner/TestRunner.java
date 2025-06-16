9ae27a4192c9f796907552923311cdbd9dd05033
Akash <akash1608.n@gmail.com>
2025-06-17 00:06:31 +0530

    removed testng dependency and reporter class - 16/06/2025

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,13 +4,14 @@ package runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import stepDefinitions.MyHooks;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "classpath:features",   // ✅ path to your .feature files
-        glue = "stepDefinitions",            // ✅ package of your step defs
+        glue = {"stepDefinitions"},          // ✅ package of your step defs
         plugin = {"pretty", "html:target/cucumber-report.html"}, // optional, for report
-        monochrome = true,tags = "@all")
+        monochrome = false,tags = "@smoke")
 public class TestRunner {
 
 
